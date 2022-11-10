1ba53995c429779fdd0ffcbce089cdebdbf230ee
Zisan Ahmed <ahmed.zisanahmed@gmail.com>
2022-11-10 21:30:10 +0600

    commit

---
+++ GymManagement/ViewEquipment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymManagement
+{
+    public partial class ViewEquipment : Form
+    {
+        public ViewEquipment()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewEquipment_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-UA2OV4B\SQLEXPRESS;Initial Catalog=gym;Integrated Security=True");
+                conn.Open();
+                string query = "select * from Equipment";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+                //dataGridView1.AutoGenerateColumns = false;
+
+                dataGridView1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
